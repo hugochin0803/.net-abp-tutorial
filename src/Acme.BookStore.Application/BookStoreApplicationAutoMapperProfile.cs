b97e524635599c5c4d2817c8c9de6e0baa6f37bf
hugochin0803 <hugochin0803@gmail.com>
2023-02-15 13:20:42 +0800

    first commit

---
+++ src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Acme.BookStore.Books;
+
+namespace Acme.BookStore;
+
+public class BookStoreApplicationAutoMapperProfile : Profile
+{
+    public BookStoreApplicationAutoMapperProfile()
+    {
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
+    }
+}
